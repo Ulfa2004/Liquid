a4946cb5c50225fee5c96297b3ed12f25c8072b9
destamusic <ulfa20040108@gmail.com>
2026-05-12 10:09:53 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;14..32,500;14..32,600;14..32,700;14..32,800;14..32,900&display=swap" rel="stylesheet" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;14..32,500;14..32,600;14..32,700;14..32,800;14..32,900&display=swap" 
+          rel="stylesheet" 
+        />
       </Head>
       <body>
         <Main />
